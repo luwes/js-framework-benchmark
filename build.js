e0d03585afcf82f356ec183e8220bc0f306b6e59
Wesley Luyten <me@wesleyluyten.com>
2019-06-29 12:41:58 -0400

    Add framework selection to build.js

--- build.js
+++ build.js
@@ -5,12 +5,15 @@ var path = require('path');
 var yargs = require('yargs');
 
 let args = yargs(process.argv)
-    .usage("npm run build [-- [--check] [--skipIrrelevant] [--restartWith] [--benchmarks_only]]")
+    .usage("npm run build [-- [--framework] [--check] [--skipIrrelevant] [--restartWith] [--benchmarks_only]]")
     .help('help')
     .boolean('check')
+    .boolean('fork')
     .boolean('benchmarks_only')
     .boolean('skipIrrelevant')
     .string('restartWith')
+    .array('framework')
+    .default('count', 1)
     .argv;
 
 var referenceBranch = "origin/master";
@@ -19,9 +22,20 @@ var restartWithFramework = args.restartWith || '';
 
 var core = args.benchmarks_only ? [] : ["webdriver-ts", "webdriver-ts-results"].map(f => ["", f]);
 
+var idMap = {};
+var keyed = args.framework.filter(f => !/-non-keyed$/.test(f));
+var nonKeyed = args.framework.filter(f => !/-keyed$/.test(f));
+
 var frameworks = [].concat(
-  fs.readdirSync('./frameworks/keyed').map(f => ['frameworks/keyed/', f]),
-  fs.readdirSync('./frameworks/non-keyed').map(f => ['frameworks/non-keyed/', f]));
+  fs.readdirSync('./frameworks/keyed')
+    .filter(f => args.framework.length === 0 ||
+        keyed.some(id => new RegExp(`^${f}`).test(id) && (idMap[f] = id)))
+    .map(f => ['frameworks/keyed/', f]),
+  fs.readdirSync('./frameworks/non-keyed')
+    .filter(f => args.framework.length === 0 ||
+      nonKeyed.some(id => new RegExp(`^${f}`).test(id) && (idMap[f] = id)))
+    .map(f => ['frameworks/non-keyed/', f])
+);
 
 var notRestarter = ([_, name]) => !name.startsWith(restartWithFramework || undefined);
 var [skippable, buildable] = !restartWithFramework
@@ -57,7 +71,7 @@ _.each(testable, function([dir,name]) {
         let fullname = dir + name;
 	if(fs.statSync(fullname).isDirectory() && fs.existsSync(path.join(fullname, "package.json"))) {
             console.log("*** Executing npm run selenium for "+fullname);
-            exec('npm run selenium -- --count 1 --fork false --framework ' + name, {
+            exec(`npm run selenium -- --count ${args.count} --headless --fork ${args.fork} --framework ${idMap[name]}`, {
 				cwd: "webdriver-ts",
 				stdio: 'inherit'
 			});
